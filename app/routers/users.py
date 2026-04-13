6a89ad59f8ffdd0c6b67ebe14668aa1eddf31803
Macey Ragbir <maceyragbir@gmail.com>
2026-04-12 22:04:38 -0400

    Part 2 InProgress CSS

--- app/routers/users.py
+++ app/routers/users.py
@@ -7,7 +7,17 @@ from app.repositories.user import UserRepository
 from app.utilities.flash import flash
 from app.schemas import UserResponse
 
+@api_router.get("/users/current")
+async def get_current_user_info(current_user: AuthDep):
+    """Get the currently logged in user's info"""
+    return {
+        "id": current_user.user_id,
+        "username": current_user.username,
+        "email": current_user.email,
+        "role": current_user.role
+    }
 
+    
 @api_router.get("/users", response_model=list[UserResponse])
 async def list_users(request: Request, db: SessionDep):
     user_repo = UserRepository(db)
